8c892c40da311426d6e0354062941802c3aa484f
absognety <cvikas10@ms.ds.uhc.com>
2018-08-19 21:09:40 +0530

    prime number Detector

--- primeNumber_Detection.py
+++ primeNumber_Detection.py
@@ -1,11 +1,17 @@
-#prime number detector (For numbers less than 9409)
+#prime number detector (For numbers less than 10201)
 
 import math
 import numpy as np
 
-prime_numbers = np.array([2,3,5,7,11.13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,83,89,97])
+prime_numbers = np.array([2,3,5,7,11.13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,83,89,97,101])
 
 def primeDetector(p):
+    
+    """Function starts here"""
+    
+    if (p > 10201):
+        raise ValueError("greater than 10201, enter a smaller number")
+    
     if p < 100 and p in prime_numbers:
         return ("{} is a prime number".format(p))
     else:
