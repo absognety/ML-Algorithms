110c46851633b604cc37e2cbc0ae0eb989f92c7f
absognety <cvikas10@ms.ds.uhc.com>
2018-08-19 20:55:44 +0530

    prime number Detector

---
+++ primeNumber_Detection.py
@@ -0,0 +1,26 @@
+#prime number detector (For numbers less than 9409)
+
+import math
+import numpy as np
+
+prime_numbers = np.array([2,3,5,7,11.13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,83,89,97])
+
+def primeDetector(p):
+    if p < 100 and p in prime_numbers:
+        return ("{} is a prime number".format(p))
+    else:
+        sq_p = math.sqrt(p)
+        near_gt = math.ceil(sq_p)
+        lessThan_num = prime_numbers[prime_numbers < near_gt]
+        final_Ans = ['No' if (p%i) == 0 else "Yes" for i in lessThan_num]
+        final_Ans = list(set(final_Ans))
+        if (len(final_Ans) == 1 and final_Ans[0] == 'Yes'):
+            return ("{} is a prime number".format(p))
+        else:
+            return ("{} is not a prime number".format(p))
+    
+if __name__ == '__main__':
+    p = int(input())
+    print (primeDetector(p))
+
+    
