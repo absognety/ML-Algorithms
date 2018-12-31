d2429dc0ec4e75b4f17c7c7fd750f44ce16fb5f4
absognety <cvikas10@ms.ds.uhc.com>
2018-12-31 21:19:53 +0530

    prime numbers list corrected

--- primeNumber_Detection.py
+++ primeNumber_Detection.py
@@ -3,7 +3,7 @@
 import math
 import numpy as np
 
-prime_numbers = np.array([2,3,5,7,11.13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,83,89,97,101])
+prime_numbers = np.array([2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,83,89,97,101])
 
 def primeDetector(p):
     
